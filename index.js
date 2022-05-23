14743e9e7a3808e479e141d464692f79d451ef79
Edmund <edmund.adjei635@gmail.com>
2022-05-23 15:22:40 -0400

    commit tree a8b926999207d1da3feba3b3d35fc01b4e3491ed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+const messages = []; 
+function writeCards(messages, event){
+    for(let i = 0; i < messages.length; i++){
+        messages[i] = `Thank you, ${messages[i]}, for the wonderful ${event} gift!`;
+    }
+    return messages;
+}
+function countDown(integer){
+    while(integer > -1){
+        console.log(integer);
+        integer--;
+    }
+}
